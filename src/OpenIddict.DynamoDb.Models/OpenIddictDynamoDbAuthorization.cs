5dc6b8f39ce8a53f8c917882ad294ffe094e4c07
Akhan Zhakiyanov <ahanoff@gmail.com>
2024-02-06 02:47:37 +0800

    fix: comments

--- src/OpenIddict.DynamoDb.Models/OpenIddictDynamoDbAuthorization.cs
+++ src/OpenIddict.DynamoDb.Models/OpenIddictDynamoDbAuthorization.cs
@@ -1,4 +1,5 @@
 using System.Collections.Immutable;
+using Amazon.DynamoDBv2.DataModel;
 
 namespace OpenIddict.DynamoDb.Models;
 
@@ -7,48 +8,13 @@ namespace OpenIddict.DynamoDb.Models;
 /// </summary>
 public class OpenIddictDynamoDbAuthorization
 {
-    /// <summary>
-    /// Gets or sets the identifier of the application associated with the current authorization.
-    /// </summary>
     public virtual required string ApplicationId { get; set; }
-
-    /// <summary>
-    /// Gets or sets the concurrency token.
-    /// </summary>
     public virtual string? ConcurrencyToken { get; set; } = Guid.NewGuid().ToString();
-
-    /// <summary>
-    /// Gets or sets the UTC creation date of the current authorization.
-    /// </summary>
     public virtual DateTime? CreationDate { get; set; }
-
-    /// <summary>
-    /// Gets or sets the unique identifier associated with the current authorization.
-    /// </summary>
     public virtual required string Id { get; set; }
-
-    /// <summary>
-    /// Gets or sets the additional properties associated with the current authorization.
-    /// </summary>
     public virtual string? Properties { get; set; }
-
-    /// <summary>
-    /// Gets or sets the scopes associated with the current authorization.
-    /// </summary>
     public virtual IReadOnlyList<string>? Scopes { get; set; } = ImmutableList.Create<string>();
-
-    /// <summary>
-    /// Gets or sets the status of the current authorization.
-    /// </summary>
     public virtual string? Status { get; set; }
-
-    /// <summary>
-    /// Gets or sets the subject associated with the current authorization.
-    /// </summary>
     public virtual string? Subject { get; set; }
-
-    /// <summary>
-    /// Gets or sets the type of the current authorization.
-    /// </summary>
     public virtual string? Type { get; set; }
 }
--- src/OpenIddict.DynamoDb.Models/OpenIddictDynamoDbToken.cs
+++ src/OpenIddict.DynamoDb.Models/OpenIddictDynamoDbToken.cs
@@ -5,73 +5,17 @@ namespace OpenIddict.DynamoDb.Models;
 /// </summary>
 public class OpenIddictDynamoDbToken
 {
-    /// <summary>
-    /// Gets or sets the identifier of the application associated with the current token.
-    /// </summary>
-    public virtual required string ApplicationId { get; set; }
-
-    /// <summary>
-    /// Gets or sets the identifier of the authorization associated with the current token.
-    /// </summary>
+    public virtual required string ApplicationId { get; set; } 
     public virtual required string AuthorizationId { get; set; }
-
-    /// <summary>
-    /// Gets or sets the concurrency token.
-    /// </summary>
     public virtual string? ConcurrencyToken { get; set; } = Guid.NewGuid().ToString();
-
-    /// <summary>
-    /// Gets or sets the UTC creation date of the current token.
-    /// </summary>
     public virtual DateTime? CreationDate { get; set; }
-
-    /// <summary>
-    /// Gets or sets the UTC expiration date of the current token.
-    /// </summary>
     public virtual DateTime? ExpirationDate { get; set; }
-
-    /// <summary>
-    /// Gets or sets the unique identifier associated with the current token.
-    /// </summary>
     public virtual required string Id { get; set; }
-
-    /// <summary>
-    /// Gets or sets the payload of the current token, if applicable.
-    /// Note: this property is only used for reference tokens
-    /// and may be encrypted for security reasons.
-    /// </summary>
     public virtual string? Payload { get; set; }
-
-    /// <summary>
-    /// Gets or sets the additional properties associated with the current token.
-    /// </summary>
     public virtual string? Properties { get; set; }
-
-    /// <summary>
-    /// Gets or sets the UTC redemption date of the current token.
-    /// </summary>
     public virtual DateTime? RedemptionDate { get; set; }
-
-    /// <summary>
-    /// Gets or sets the reference identifier associated
-    /// with the current token, if applicable.
-    /// Note: this property is only used for reference tokens
-    /// and may be hashed or encrypted for security reasons.
-    /// </summary>
     public virtual string? ReferenceId { get; set; }
-
-    /// <summary>
-    /// Gets or sets the status of the current token.
-    /// </summary>
     public virtual string? Status { get; set; }
-
-    /// <summary>
-    /// Gets or sets the subject associated with the current token.
-    /// </summary>
     public virtual string? Subject { get; set; }
-
-    /// <summary>
-    /// Gets or sets the type of the current token.
-    /// </summary>
     public virtual string? Type { get; set; }
 }
